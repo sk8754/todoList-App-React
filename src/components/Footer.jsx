b26a36e3ef3d3a10a6a029cfbbed5c2c369b4b3e
sk8754 <kishi8755@gmail.com>
2024-06-19 21:47:23 +0900

    更新

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ function Footer() {
     <div>
       <footer>
         <div className="footerContent">
-          <a href="#">todoList.com</a>
-          <a href="#">programs</a>
-          <a href="#">TOP</a>
+          <a href="/">todoList.com</a>
+          <a href="/">programs</a>
+          <a href="/">TOP</a>
         </div>
       </footer>
     </div>
